b60f53ed136ba73045cb2859097cbc220d8ec783
Hans-Fredrik <hansfredrik@outlook.com>
2015-11-28 11:22:13 +0100

    UserController 100% done

--- src/test/java/controller/LocationControllerTest.java
+++ src/test/java/controller/LocationControllerTest.java
@@ -1,44 +1,89 @@
 package controller;
 
+import domain.Course;
+import domain.Location;
+import domain.User;
 import org.junit.Before;
 import org.junit.Test;
+import repository.CourseDAO;
+import repository.LocationDAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 public class LocationControllerTest {
 
+    private LocationController locationController;
+    private LocationDAO locationDAO;
+    private CourseDAO courseDAO;
+
     @Before
     public void setUp() throws Exception {
-
+        locationDAO = mock(LocationDAO.class);
+        courseDAO = mock(CourseDAO.class);
+        locationController = new LocationController(locationDAO, courseDAO);
+        locationController.init();
     }
 
     @Test
     public void testGetLocation() throws Exception {
-
+        assertNotNull(locationController.getLocation());
     }
 
     @Test
     public void testPersist() throws Exception {
-
+        locationController.persist();
+        verify(locationDAO, times(1)).persist(new Location());
     }
+
 
     @Test
     public void testDelete() throws Exception {
+        List<Course> courseList = new ArrayList<>();
+        Location location1 = new Location("TestRoom", "TestBuildning");
+        location1.setId(1);
 
+        Course course = new Course();
+        course.setLocation(location1);
+        courseList.add(course);
+
+        when(courseDAO.getAll()).thenReturn(courseList);
+        locationController.delete(location1);
+        verify(courseDAO, times(1)).getAll();
+        verify(locationDAO, times(1)).remove(location1);
     }
+
 
     @Test
     public void testUpdate() throws Exception {
-
+        Location location = new Location();
+        locationController.update(location);
+        verify(locationDAO, times(1)).update(location);
     }
+
 
     @Test
     public void testGetAll() throws Exception {
-
+        locationController.getAll();
+        verify(locationDAO, times(1)).getAll();
     }
+
 
     @Test
     public void testGetAllLocationsIfCourseDontHaveLocation() throws Exception {
+        Course course = new Course();
+        course.setLocation(new Location());
 
+        assertNotNull(locationController.getAllLocationsIfCourseDontHaveLocation(course));
+        assertEquals(0,locationController.getAllLocationsIfCourseDontHaveLocation(course).size());
+
+        course.setLocation(null);
+
+        when(locationController.getAllLocationsIfCourseDontHaveLocation(course)).thenReturn(new ArrayList<Location>());
+        assertNotNull(locationController.getAllLocationsIfCourseDontHaveLocation(course));
+        verify(locationDAO, times(1)).getAll();
     }
 }
---
+++ src/test/java/controller/UserControllerTest.java
@@ -0,0 +1,87 @@
+package controller;
+
+import domain.User;
+import domain.UserType;
+import org.junit.Before;
+import org.junit.Test;
+import repository.UserDAO;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    private UserDAO userDAO;
+    private UserController userController;
+
+    @Before
+    public void setUp() throws Exception {
+        userDAO = mock(UserDAO.class);
+        userController = new UserController(userDAO);
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+        userController.init();
+        assertNotNull(userController.getUser());
+    }
+
+    @Test
+    public void testPersist() throws Exception {
+        userController.persist();
+        verify(userDAO, times(1)).persist(anyObject());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        User user = new User();
+        userController.delete(user);
+        verify(userDAO, times(1)).remove(user);
+    }
+
+    @Test
+    public void testUpdateFail() throws Exception {
+        final int userID = 1;
+        assertEquals("users", userController.update(userID));
+        when(userDAO.findById(userID)).thenReturn(null);
+        verify(userDAO, times(1)).findById(userID);
+        verify(userDAO, never()).update(anyObject());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        final int userID = 2;
+        userController.init();
+        userController.getUser().setEmail("Test@Test.no");
+        userController.getUser().setPassword("test123T");
+        userController.getUser().setUserType(UserType.TEACHER);
+
+        when(userDAO.findById(userID)).thenReturn(new User());
+        assertEquals("users", userController.update(userID));
+        verify(userDAO, times(1)).findById(userID);
+        verify(userDAO, times(1)).update(anyObject());
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        when(userController.getAll()).thenReturn(new ArrayList<User>());
+        assertNotNull(userController.getAll());
+        verify(userDAO, times(1)).getAll();
+    }
+
+    @Test
+    public void testInitUser() throws Exception {
+        userController.setSelectedId(1);
+        userController.initUser();
+        verify(userDAO, times(1)).findById(1);
+    }
+
+    @Test
+    public void testSetGetSelectedId() throws Exception {
+        userController.setSelectedId(1);
+        assertEquals(1, userController.getSelectedId());
+    }
+}
